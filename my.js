16eef8f94fe216f0a467b5a65f2d22805b9ce3de
Tiffany Kuo <24443506+tiffak@users.noreply.github.com>
2018-08-07 09:36:02 -0400

    Added JS files

---
+++ my.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 50});   
+
+  $("#navbarCollapse a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+$(document).ready(function(){
+  $('body').scrollspy({target: "#down", offset: 50});   
+
+  $("#down").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
